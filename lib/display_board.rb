32cdcea66999596c62c9c55cde63feb155fd1720
Amanda Ackerman <ackerm44@gmail.com>
2017-10-13 01:56:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "   ""|"""   ""|"""   "
+  puts "   "|"   "|"   "
   puts "-----------"
   puts "   ""|"""   ""|"""   "
   puts "-----------"
@@ -8,5 +8,3 @@ def display_board
 end
 
 display_board
-
-
